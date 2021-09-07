429da3f1d3cc4921f1158cd39ce73a31edd8938b
Yourfriendsudha <chandira93@gmail.com>
2021-09-07 22:09:55 +0530

    changed

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome, Simple flask api . this is version 2"
+    return "<h1>Welcome, Simple flask api . this is version 3. Rolling strategy tested</h1>"
+
+@app.route('/home', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
 app.run(host="0.0.0.0", port=8080, debug=True)
