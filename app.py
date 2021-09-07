344f6ab62cf27556a9cab45357e27483ae2fd980
Yourfriendsudha <chandira93@gmail.com>
2021-09-07 17:57:44 +0530

    changes

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome, Simple flask api"
+    return "Welcome, Simple flask api . this is version 2"
 app.run(host="0.0.0.0", port=8080, debug=True)
